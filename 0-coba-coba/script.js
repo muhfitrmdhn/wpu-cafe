9f0e4a6cb30729234480c89eb3f0abf79172128a
muhfitmrdhn <fitrahramadhan846@gmail.com>
2026-09-20 23:56:10 +0800

    feat: add additional JavaScript examples for demonstration and loop implementation

--- 0-coba-coba/script.js
+++ 0-coba-coba/script.js
@@ -38,3 +38,66 @@
 // console.log(a * b);
 // console.log(a / b);
 // console.log(a % b);
+
+// const buah = ["jeruk", "pepaya"];
+// buah.push("anggur");
+
+// console.log(buah);
+
+// let umur = 17;
+// if (umur >= 18) {
+//   console.log("Sudah Dewasa");
+// } else {
+//   console.log("Belum Dewasa");
+// }
+
+// let nilai = 70;
+// if (nilai >= 90) {
+//   console.log("A");
+// } else if (nilai >= 80) {
+//   console.log("B");
+// } else if (nilai >= 70) {
+//   console.log("C");
+// } else {
+//   console.log("D");
+// }
+
+// let umur = 16;
+// let punyaKTP = true;
+
+// if (umur >= 17 && punyaKTP === true) {
+//   console.log("Boleh masuk");
+// } else {
+//   console.log("Tidak Boleh Masuk");
+// }
+
+// let hari = "Senin";
+
+// if (hari === "Sabtu" || hari === "Minggu") {
+//   console.log("Weekend");
+// } else {
+//   console.log("Kerja Bro!");
+// }
+
+// let hari = "Jumat";
+
+// switch (hari) {
+//   case "Senin":
+//     console.log("Hari pertama");
+//     break;
+
+//   case "Selasa":
+//     console.log("Hari kedua");
+//     break;
+
+//   case "Rabu":
+//     console.log("Hari ketiga");
+//     break;
+
+//   default:
+//     console.log("Hari tidak ditemukan");
+// }
+
+// for (let i = 1; i <= 50; i++) {
+//   console.log(i);
+// }
