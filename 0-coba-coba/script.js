b0d7951d060e7cf3558d57a45e2c312c675af79c
muhfitmrdhn <fitrahramadhan846@gmail.com>
2026-09-20 22:06:46 +0800

    feat: add initial HTML and JavaScript files for variable and function demonstration

---
+++ 0-coba-coba/script.js
@@ -0,0 +1,40 @@
+// let sayur = ["bayam", "kangkung", "kelor", "jahe"];
+
+// let motor = {
+//   merek: "Honda",
+//   cc: "150cc",
+//   warna: "merah",
+// };
+
+// console.log(sayur[2]);
+// console.log(motor.warna);
+
+// let siswa = [
+//   { nama: "fahmi", umur: 17, agama: "islam" },
+//   {
+//     nama: "anugrah",
+//     umur: 18,
+//     agama: "islam",
+//   },
+
+//   { nama: "hamdani", umur: 17, agama: "katholik" },
+// ];
+
+// console.log(siswa[2]);
+
+// let nama;
+
+// console.log(nama);
+
+// let namaTengah = null;
+
+// console.log(namaTengah);
+
+// let a = 20;
+// let b = 6;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
