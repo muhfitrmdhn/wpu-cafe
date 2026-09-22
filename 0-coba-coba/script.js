8658e70f0ee5a7d7c17d217e210abe5ae15ce0b8
muhfitmrdhn <fitrahramadhan846@gmail.com>
2026-09-22 11:04:29 +0800

    feat: add additional functions and examples for asynchronous operations and array filtering

--- 0-coba-coba/script.js
+++ 0-coba-coba/script.js
@@ -101,3 +101,108 @@
 // for (let i = 1; i <= 50; i++) {
 //   console.log(i);
 // }
+
+// function sapa(nama) {
+//   console.log("halo" + nama);
+// }
+
+// sapa(" Hamdani");
+// sapa(" Asrul");
+// sapa(" Rahim");
+
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// let hasil = tambah(10, 15);
+// console.log(hasil);
+
+// const tambah = (a, b) => a + b;
+// let hasil = tambah(15, 15);
+// console.log(hasil);
+
+// let siswa = {
+//   nama: "Putra",
+
+//   sapa: function () {
+//     console.log("Halo");
+//   },
+// };
+
+// siswa.sapa();
+
+// function sapa(nama) {
+//   console.log("Halo, " + nama);
+// }
+
+// function proses(callback) {
+//   callback("Fitrah");
+// }
+
+// proses(sapa);
+
+// function ambilNama() {
+//   return Promise.resolve("Fitrah");
+// }
+
+// async function tampilkanNama() {
+//   const nama = await ambilNama();
+
+//   console.log(nama);
+// }
+
+// tampilkanNama();
+
+// async function ambilUsers() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+//   const users = await response.json();
+
+//   const email = users.map((users) => users.email);
+
+//   console.log(email);
+// }
+
+// ambilUsers();
+
+// async function ambilData() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+//     const data = await response.json();
+
+//     console.log(data);
+//   } catch (error) {
+//     console.log("Terjadi error:", error);
+//   }
+// }
+
+// ambilData();
+
+// const input = document.querySelector("#input");
+// const tombol = document.querySelector("#tambah");
+// const list = document.querySelector("#list");
+
+// tombol.addEventListener("click", () => {
+//   const teks = input.value;
+
+//   const li = document.createElement("li");
+
+//   li.textContent = teks;
+
+//   list.append(li);
+
+//   input.value = "";
+// });
+
+const angka = [1, 2, 3, 4, 5];
+
+const hasil = [];
+
+for (let i = 0; i < angka.length; i++) {
+  if (angka[i] % 2 === 0) {
+    hasil.push(angka[i]);
+  }
+}
+
+console.log(hasil);
