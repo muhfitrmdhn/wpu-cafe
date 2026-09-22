4cf2938b03151a909cec4763065f6a5a39a5a2ac
muhfitmrdhn <fitrahramadhan846@gmail.com>
2026-09-22 14:38:45 +0800

    feat: initialize React project with Vite, add global styles and SVG assets
    - Added main entry point for React application in `main.tsx`.
    - Created global styles in `globals.css`.
    - Introduced TypeScript configuration files for app and node environments.
    - Added SVG assets for React and Vite logos.
    - Configured Vite with React plugin in `vite.config.ts`.

---
+++ web-create/src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react/jsx-runtime";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>
+        Hello <i>World!</i>
+      </h1>
+      <button>submit</button>
+    </Fragment>
+  );
+};
+
+export default App;
